87d76370ca0d50e1f3a04b7544e5b8db0bbc2833
Fan Wenbo <wbfan@thoughtworks.com>
2015-02-12 15:11:43 +0800

    a example for creating form, please break this to angular template in the future.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/form', function(req, res, next) {
+  res.render('form');
+});
+
 module.exports = router;
