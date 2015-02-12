01cf4f5ef01223bdc4f862b67ca3e87c8d216acc
Fan Wenbo <wbfan@thoughtworks.com>
2015-02-12 15:27:32 +0800

    change angular template from html to jade.

--- routes/templates.js
+++ routes/templates.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:template', function(req, res, next) {
+  res.render('templates/' + req.params.template);
 });
 
 module.exports = router;
