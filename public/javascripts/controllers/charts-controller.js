78139a29cded8cf6576a0a1ccd9b8de9eb74a09f
Liu Jing <liujing92684@gmail.com>
2015-02-12 14:29:28 +0800

    init angular app

--- public/javascripts/controllers/charts-controller.js
+++ public/javascripts/controllers/charts-controller.js
@@ -1,6 +1,12 @@
-angular.module('metric').controller('ChartsController',
-    ['$scope', '$timeout',
-        function ($scope, $timeout) {
-            $scope.allData = {};
+(function (angular, $, moment) {
 
-        }]);
+    angular.module('metric').controller('ChartsController',
+        ['$scope', '$timeout',
+            function ($scope, $timeout) {
+                $scope.allData = {};
+
+                function xDataFor(date) {
+                    return moment(date).startOf('year').toDate();
+                }
+            }]);
+}(window.angular, window.jQuery, window.moment));
