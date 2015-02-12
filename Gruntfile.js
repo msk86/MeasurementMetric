8e027c91ed56caa632f0a79c1f246ed983ae3de3
Sarah <sixiaojing1988@gmail.com>
2015-02-12 15:33:26 +0800

    homepage layout

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function(grunt) {
         sass: {
             dist: {
                 files: {
-                    'public/stylesheets/style.css' : 'sass/style.scss'
+                    'public/stylesheets/style.css' : 'sass/application.scss'
                 }
             }
         },
