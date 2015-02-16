0343602191f3ea8912e1e1e1c6b7407c7ed1c134
Fan Wenbo <wbfan@thoughtworks.com>
2015-02-16 15:35:28 +0800

    remove test page and moco

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Measurement Metric' });
 });
 
-
-// For test
-router.get('/create-metric', function(req, res, next) {
-  res.render('metric-create');
-});
-router.get('/record-metric', function(req, res, next) {
-  res.render('metric-record');
-});
-
 module.exports = router;
--- scripts/start-moco.sh
+++
@@ -1,1 +0,0 @@
-java -Xmx256m -jar ./mock-data/lib/moco-runner-0.9.1-standalone.jar start -p 3000 -c ./mock-data/default.json
