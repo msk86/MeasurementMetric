01ad95115b4246d070be6571ecb20aa838abab26
Fan Wenbo <wbfan@thoughtworks.com>
2015-03-03 15:08:05 +0800

    add a trick to prevent showing all trends from the chart

--- models/metric.js
+++ models/metric.js
@@ -162,7 +162,9 @@ module.exports = (function() {
                         value[g] = rangeReduceData(gm, ranges, processMethod);
                     });
                 }
-                value.all = rangeReduceData(metricData, ranges, processMethod);
+                if(!settings.metricTypes.length || (settings.metricTypes.length >= 2 && !settings.excludeAllTrends)) {
+                    value.all = rangeReduceData(metricData, ranges, processMethod);
+                }
 
                 var data = {
                     metricName: metricName,
